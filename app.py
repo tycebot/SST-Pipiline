a3e70e484a1ff14f0bf405b3be8c62ebc32114e9
tycebot <89507652+tycebot@users.noreply.github.com>
2025-09-06 00:18:36 -0700

    added space to download name

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def main():
     #Download button for data
     st.download_button("Download Data", 
     df.to_csv(index=False), 
-    file_name=station + start_date + '_to_' + end_date +
+    file_name=station + '_' + start_date + '_to_' + end_date +
     '.csv')
     
     # Download button for static plot   
